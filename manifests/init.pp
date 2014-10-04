9366cbabb1f8e0b15071297e7f6d4dd4df2da972
Jonathan Araña Cruz <jonhattan@faita.net>
2014-10-04 12:04:16 +0200

    Add extra condition to collectors to match the resource's creator.

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,20 +8,21 @@ class webapp(
 
   if $autorealize {
     if defined('::apache') and defined(Class['::apache']) {
-      Apache::Vhost  <<| tag == $::fqdn |>>
-      Host           <<| tag == $::fqdn |>>
+      Apache::Vhost  <<| tag == $::fqdn and tag == webapp::instance |>>
+      Host           <<| tag == $::fqdn and tag == webapp::instance |>>
+      File           <<| tag == $::fqdn and tag == webapp::instance |>>
     }
 
     if defined('::drush') and defined(Class['::drush']) {
-      Drush::Alias   <<| tag == $::fqdn |>>
+      Drush::Alias   <<| tag == $::fqdn and tag == webapp::instance |>>
     }
 
     if defined('::mysql::server') and defined(Class['::mysql::server']) {
-      Mysql::Db      <<| tag == $::fqdn |>>
+      Mysql::Db      <<| tag == $::fqdn and tag == webapp::instance |>>
     }
 
     if defined('::solr') and defined(Class['::solr']) {
-      Solr::Instance <<| tag == $::fqdn |>>
+      Solr::Instance <<| tag == $::fqdn and tag == webapp::instance |>>
     }
   }
 }
